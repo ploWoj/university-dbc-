644538133fd0004cc436c56d13cc3f0cf9cf2f7e
Wojtek <wojtek.plociennik@gmail.com>
2023-03-27 11:37:38 -0700

    Person.hpp

---
+++ person.hpp
@@ -0,0 +1,42 @@
+#pragma once 
+#include <string>
+
+#include "address.hpp"
+
+enum class Sex {
+    Femel, Male, Other
+};
+
+class Person {
+    enum class PersonType {
+        Student, Employee
+    };
+
+    Person(PersonType type, std::string firstName, std::string secondName, std::string pesel, Address address, Sex sex);
+
+    virtual ~Person();
+
+    // getters
+
+    PersonType getPersonType() const;
+    std::string getFirstName() const;
+    std::string getSecondName() const;
+    std::string getPesel() const;
+    std::string getAddress() const;
+    std::string getPostalCode() const;
+    std::string getCity() const;
+    std::string getStreetAndNumber() const;
+
+    Sex getSex() const;
+    virtual size_t getSalary() const = 0;
+    virtual size_t getIndexNumber() const = 0;
+    virtual void setSalary(const size_t& newSalary) = 0;
+
+public: 
+    PersonType type_;
+    std::string firstName_;
+    std::string secondName_;
+    std::string pesel_;
+    Address address_;
+    Sex sex_;   
+};
