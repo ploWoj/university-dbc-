a5a1073754ecaea843244502ee75f05e4e4d7a03
Wojtek <wojtek.plociennik@gmail.com>
2023-03-28 05:54:53 -0700

    implementation validationPesel

--- university.cpp
+++ university.cpp
@@ -1,8 +1,42 @@
 #include "university.hpp"
 
+#include <cctype>
+
 const DB University::getDB() const {
     return university_;
 }
+
+bool University(const std::string& pesel) {
+    if (pesel.size() != peselSize) {
+        return false;
+    }
+
+    constexpr std::array<int,weightNumbers> weighsFactor  { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+
+    size_t checkSum{0};
+    size_t number{0};
+
+    for (size_t i = 0; i < weightNumbers; ++i) {
+        if (!std::isdigit(pesel[i])) {
+            return false;
+        }
+
+        number = pesel[i] - '0';
+        checkSum = number * weighsFactor[i];
+    }
+
+    checkSum = checkSum % weightNumbers;
+
+    if (checkSum != 0) {
+        checkSum = weightNumbers - checkSum;
+    }
+
+    size_t lastNumber = pesel.back() - '0';
+
+    return checkSum == lastNumber;
+}
+
+
 
 void University::displayDB() {
     for (const auto& person : university_) {
--- university.hpp
+++ university.hpp
@@ -1,5 +1,6 @@
 #pragma once 
 
+#include <array>
 #include <memory>
 #include <vector>
 
@@ -7,6 +8,8 @@
 #include "student.hpp"
 
 using  DB =  std::vector<std::unique_ptr<Person>>;
+constexpr const size_t peselSize = 11;
+constexpr const size_t weightNumbers = 10;
 
 class University {
 public:
@@ -14,9 +17,19 @@ public:
     ~University() = default;
 
     const DB getDB() const;
-
+    void displayDB();
+    void addStudent(const std::string& name, const std::string& surname, const std::string& address, const std::string& pesel, Sex& sex, size_t indexNumber);
+//     Przechowywanie rekordów studentów o strukturze: Imię, nazwisko, adres, nr indeksu, PESEL, płeć
+// Dodawanie nowych studentów
+// Wyświetlanie całej bazy danych - zrobione
+// Wyszukiwanie po nazwisku
+// Wyszukiwanie po numerze PESEL
+// Sortowanie po numerze PESEL
+// Sortowanie po nazwisku
+// Usuwanie po numerze indeksu
 
 
 private:
+    bool peselValidation(const std::string& pesel);
     DB university_; 
 };
