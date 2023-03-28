8f11ebd04ad548ca7fe5044f2aac711f37bd54cf
Wojtek <wojtek.plociennik@gmail.com>
2023-03-28 06:24:56 -0700

    IMplementation of findByPesel

--- university.cpp
+++ university.cpp
@@ -1,23 +1,31 @@
 #include "university.hpp"
 
+#include <algorithm>
 #include <cctype>
 
-const DB University::getDB() const {
+#include <utility>
+
+const DB University::getDB() const
+{
     return university_;
 }
 
-bool University(const std::string& pesel) {
-    if (pesel.size() != peselSize) {
+bool University(const std::string &pesel)
+{
+    if (pesel.size() != peselSize)
+    {
         return false;
     }
 
-    constexpr std::array<int,weightNumbers> weighsFactor  { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+    constexpr std::array<int, weightNumbers> weighsFactor{1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
 
     size_t checkSum{0};
     size_t number{0};
 
-    for (size_t i = 0; i < weightNumbers; ++i) {
-        if (!std::isdigit(pesel[i])) {
+    for (size_t i = 0; i < weightNumbers; ++i)
+    {
+        if (!std::isdigit(pesel[i]))
+        {
             return false;
         }
 
@@ -27,7 +35,8 @@ bool University(const std::string& pesel) {
 
     checkSum = checkSum % weightNumbers;
 
-    if (checkSum != 0) {
+    if (checkSum != 0)
+    {
         checkSum = weightNumbers - checkSum;
     }
 
@@ -36,10 +45,39 @@ bool University(const std::string& pesel) {
     return checkSum == lastNumber;
 }
 
-
-
-void University::displayDB() {
-    for (const auto& person : university_) {
+void University::displayDB()
+{
+    for (const auto &person : university_)
+    {
         person->display();
     }
+}
+
+void University::addStudent(const std::string &name, const std::string &surname, const std::string &address, const std::string& pesel, Sex &sex, size_t indexNumber) {
+    if (peselValidation(pesel)) {
+        university_.emplace_back(std::make_unique<Student>(name, surname, address, pesel, sex, indexNumber));
+    } else {
+        std::cout << "Wrong pesel!!!" << '\n';
+    }
+}
+
+// std::unique_ptr<Student> University::findByPesel(const std::string& pesel) {
+//     auto find = std::find_if(university_.cbegin(), university_.cend(), [pesel](const std::unique_ptr<Student>& student) { return student->getPesel() == pesel; });
+
+//     if (find == university_.cbegin()) {
+//         return nullptr;
+//     }
+
+//     return std::move(find);
+// }
+
+std::optional<std::unique_ptr<Student>> findByPesel(const std::string& pesel) {
+    std::optional<std::unique_ptr<Student>> ptr_out{};
+    for (const auto& person : university) {
+        if (person->getPesel() == pesel) {
+            ptr_out = std::move(person);
+            return ptr_out;
+        } 
+    }
+    return ptr_out;
 }
--- university.hpp
+++ university.hpp
@@ -1,35 +1,39 @@
-#pragma once 
+#pragma once
 
 #include <array>
 #include <memory>
+#include <optional>
 #include <vector>
 
 #include "employee.hpp"
 #include "student.hpp"
 
-using  DB =  std::vector<std::unique_ptr<Person>>;
+using DB = std::vector<std::unique_ptr<Person>>;
 constexpr const size_t peselSize = 11;
 constexpr const size_t weightNumbers = 10;
 
-class University {
+class University
+{
 public:
     University() = default;
     ~University() = default;
 
     const DB getDB() const;
     void displayDB();
-    void addStudent(const std::string& name, const std::string& surname, const std::string& address, const std::string& pesel, Sex& sex, size_t indexNumber);
-//     Przechowywanie rekordów studentów o strukturze: Imię, nazwisko, adres, nr indeksu, PESEL, płeć
-// Dodawanie nowych studentów
-// Wyświetlanie całej bazy danych - zrobione
-// Wyszukiwanie po nazwisku
-// Wyszukiwanie po numerze PESEL
-// Sortowanie po numerze PESEL
-// Sortowanie po nazwisku
-// Usuwanie po numerze indeksu
-
+    void addStudent(const std::string &name, const std::string &surname, const std::string &address, const std::string &pesel, Sex &sex, size_t indexNumber);
+    //     Przechowywanie rekordów tów o strukturze: Imię, nazwisko, adres, nr indeksu, PESEL, płeć
+    // Dodawanie nowych studentów
+    // Wyświetlanie całej bazy danych - zrobione
+    std::unique_ptr<Student> findBySurname(const std::string& surname);
+    // Wyszukiwanie po nazwisku
+    std::unique_ptr<Student> findByPesel(const std::string& pesel);
+    std::optional<std::unique_ptr<Student>> findByPesel(const std::string& pesel);
+    // Wyszukiwanie po numerze PESEL
+    // Sortowanie po numerze PESEL
+    // Sortowanie po nazwisku
+    // Usuwanie po numerze indeksu
 
 private:
-    bool peselValidation(const std::string& pesel);
-    DB university_; 
+    bool peselValidation(const std::string &pesel);
+    DB university_;
 };
