8ab7b48f1895542cab457a233c21c2e6452216e0
Wojtek <wojtek.plociennik@gmail.com>
2023-03-28 04:37:41 -0700

    Finsh implementing employee.cpp

---
+++ employee.cpp
@@ -0,0 +1,18 @@
+#include "employee.hpp"
+
+Employee::Employee(std::string name, std::string surname, std::string address, std::string pesel, Sex sex, double salary) 
+    : Person(name, surname, address, pesel, sex)
+    , salary_(salary) {}
+
+void Employee::setSalary(const double salary) {
+    salary_ = salary;
+}
+
+double Employee::getSalary() const {
+    return salary_;
+}
+
+void Employee::display() {
+    std::cout << "Employee: " << Employee::getName() << ", " << Employee::getSurname() << ", " << Employee::getAddress() <<
+            Employee::getPesel() << ", " << Employee::getSex() << ", " <<  salary_ << '\n';
+}
